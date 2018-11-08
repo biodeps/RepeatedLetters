44cf592cbc76aa5498a27bfd428909bade748000
biodeps <albertokms@gmail.com>
2018-11-08 09:06:28 +0100

    Mod: Cleaned code part where "Scanner" method was not closed.

--- src/phase1/Main_Phase1.java
+++ src/phase1/Main_Phase1.java
@@ -9,11 +9,12 @@ public class Main_Phase1 {
 		Scanner inputData = new Scanner(System.in);
 		System.out.println("Good morning! Please, insert your Name");
 		String myname = inputData.nextLine();
-		System.out.print(myname.charAt(0));
+		System.out.print(myname.charAt(0)); // Afegeix coherència als espais del resultat
 		for (int i = 1; i < myname.length(); i++) {
 			System.out.print(" " + myname.charAt(i));
+		inputData.close(); // cal tancar els Scanners oberts quan ja no es necessiten
 		}
-		
+
 	}
 
 }
